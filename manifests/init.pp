7b7770539f6c2ece08385df41985b39fc634ec7f
Glenn Y. Rolland <glenux@glenux.net>
2012-12-24 01:59:44 +0100

    Improved init.

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,26 +35,87 @@
 #
 # Copyright 2011 Your name here, unless otherwise noted.
 #
-class gnuside-lxc {
+class lxc {
+    include lxc::host
 }
 
-class gnuside-lxc::host {
-    # install lxc
-    
-    # replace:
-    # SUITE=${SUITE:-lenny}
-    # with
-    # SUITE=${SUITE:-squeeze}
 
-    # replace
-    # dhcp3-client,\
-    # with
-    # isc-dhcp-client,\
+class lxc::host {
+    include lxc::host::install
+# install lxc
+
+# replace:
+# SUITE=${SUITE:-lenny}
+# with
+# SUITE=${SUITE:-squeeze}
+
+# replace
+# dhcp3-client,\
+# with
+# isc-dhcp-client,\
+}
+class lxc::host::install {
+    package { 'debootstrap': ensure => latest }
+    package { 'lxc': ensure => latest }
 }
 
-class gnuside-lxc::container {
+define lxc::container(
+	$ensure     = running
+
+	if !$host {
+	    $manage = true
+	} elsif $host == $hostname {
+	    $manage = true
+	} else {
+	    $manage = false
+	}
+	if $manage {                                                            
+	    case $ensure {
+		'running': { 
+		}
+		'stopped': { 
+		}
+		'absent': {
+		}
+	    }
+	}
+
+) {
+# FIXME: test if host = hostname
+}
+
+define lxc::container::autostart {
+}
+
+define lxc::container::state {
+}
+
+define lxc::container::create(
+	$template
+	) {
+    exec {
+	"lxc-create -n ${name} -t ${template}":
+	    onlyif  => "test ! -d /var/lib/lxc/${name}",
+		    require => Class[ 'lxc::host::install' ];
+    }
+}
+
+# Ensures, that a container is destroyed
+define lxc::container::destroy() {
+    exec {
+	"lxc-destroy -f -n ${name}":
+	    onlyif => "test -d /var/lib/lxc/${name}",
+		   require => Class[ 'lxc::host::install' ];
+    }
 }
 
 ## READ also : http://foaa.de/old-blog/2010/05/lxc-on-debian-squeeze/
 ## READ : http://www.fiat-tux.fr/fr/2011/04/lxc-sur-debian/
 
+## FIXME: use system specific debootstrap-like
+class lxc::host::install {
+    package {
+	"lxc": ensure => latest;
+	"debootstrap": ensure => latest;
+    }
+}
